b156faaa38a85d68a58e20728a56d2955658a672
Damian Cop <dcop@ec.localhost>
2019-07-05 09:36:40 +0200

    asda

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    console.log('GET  request');
+    res.send('Hello World!')}
+    );
+
+app.get('/crash', function(req, res){
+    console.log('Crashed!');
+    var filesArr = [];
+
+    fs.readdir(path.join(__dirname, 'public'), (err, files) => {
+        files.forEach(file => {
+            filesArr.push(file.toString());
+            console.log(file);
+        })
+        console.log('First elem: ' + filesArr[0]);
+        res.json({ Files: filesArr });
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
